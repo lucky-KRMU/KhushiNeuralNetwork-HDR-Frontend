88e040fd24ddc55c3d8d8903401747f479a8d77d
lucky-KRMU <2501730142@krmu.edu.in>
2026-06-30 17:55:14 +0530

    change API url

--- src/Components/Playground/Playground.jsx
+++ src/Components/Playground/Playground.jsx
@@ -121,7 +121,7 @@ export default function Playground() {
 
     const predictDigit = async (pixelArray) => {
         try {
-            const response = await fetch('http://127.0.0.1:5000/predict', {
+            const response = await fetch('https://khushineuralnetwork-hdr-backend.onrender.com/predict', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
