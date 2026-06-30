bf83b4e5e600d6491813c7cfefbc07be6ac8498b
lucky-KRMU <2501730142@krmu.edu.in>
2026-06-30 05:14:44 +0000

    add header component

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+    <header className='h-18 w-full bg-red-100 font-[Inter] font-bold flex items-center justify-center'>
+    <h1 className='text-center text-4xl text-fuchsia-400 cursor-pointer'>KNN - HDR</h1>
+    </header>
+    </>
+  )
+}
+
+export default Header
