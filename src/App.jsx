db60f1b43dd84862568a1b09c065af67bdcd9930
lucky-KRMU <2501730142@krmu.edu.in>
2026-06-30 05:55:53 +0000

    add Playground Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Header from './Components/Header/Header'
+import Playground from './Components/Playground/Playground'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Header/>
+      <Playground />
     </>
   )
 }
---
+++ src/Components/Playground/Playground.jsx
@@ -0,0 +1,9 @@
+import react from "react";
+
+export default function Playground () {
+    return(
+        <>
+        I'm Playground
+        </>
+    );
+}
